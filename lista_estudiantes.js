8a09fba6e6124c981fe8ea111a7cd6ace9b07d60
thiago2109 <sansalonemonicas@gmail.com>
2023-06-09 05:31:28 -0300

    first commit

---
+++ lista_estudiantes.js
@@ -0,0 +1,8 @@
+const MostrarListaEstudiantes = (estudiantes) => {
+  console.log("Lista de estudiantes registrados: ");
+  estudiantes.forEach((estudiante) => {
+    console.log(`Nombre: ${estudiante.nombre}, Edad: ${estudiante.edad} `);
+  });
+};
+
+module.exports = { MostrarListaEstudiantes };
